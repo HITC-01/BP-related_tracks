7791cdb3cd3a4b4a1d129a0502ac3671ba36badb
Brenden <brenden.pennington@gmail.com>
2018-10-25 17:23:25 -0700

    Add a component to test React functionality

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
+var bodyParser = require('body-parser');
 var path = require('path');
-var db = require('../database/index.js');
+
 var port = 3002;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.listen(`Listening on port ${port}`);
+app.listen(port, function () {
+    console.log(`listening on port ${port}`);
+});
 
-module.exports = db;
