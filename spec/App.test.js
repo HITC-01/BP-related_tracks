a7840d8ac59ac990170f604af8ccfd5eb29d5a0a
Brenden <brenden.pennington@gmail.com>
2018-11-05 13:50:49 -0800

    Update snapshots

--- spec/App.test.js
+++ spec/App.test.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from './setup';
-import { mount } from './setup';
 import 'isomorphic-fetch';
-import App from '../client/src/components/App';
+import SCRelated from '../client/src/components/SCRelated';
 
-describe('<App />', () => {
+describe('<SCRelated />', () => {
   test('Should render correctly', () => {
-    const component = renderer.create(<App url="http://localhost:3002" />);
+    const component = renderer.create(<SCRelated url="http://localhost:3002" />);
     const tree = component.toJSON();
 
     expect(tree).toMatchSnapshot();
   });
-
-  // test('adds songs to state', () => {
-  //   const wrapper = mount(<App url="http://localhost:3002" />);
-  //   const currentState = wrapper.debug();
-  //   console.log(currentState);
-  //   console.log(wrapper.state());
-  //   expect(wrapper.find('.songDataPlaceholder')).toBeDefined();
-  // });
-
-
 });
